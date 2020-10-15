0f920a7295ea4587a304628398468188b5dce42d
Teddy <teddy.teh@ufinity.com>
2020-10-16 03:43:57 +0800

    Updated blog base url

--- js/blogPostFetcher.js
+++ js/blogPostFetcher.js
@@ -4,8 +4,12 @@ fetch(RSS_URL)
   .then(response => response.text())
   .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
   .then(data => {
-    const latestEntry = data.querySelectorAll("entry")[0];
+    const blogBaseUrl = data.querySelectorAll("link")[1];
+    if (blogBaseUrl) {
+        $("#blogBaseUrl").attr("href", blogBaseUrl);
+    }
 
+    const latestEntry = data.querySelectorAll("entry")[0];
     if (latestEntry) {
         const title = latestEntry.querySelector("title").innerHTML;
         const link = latestEntry.querySelector("link").getAttribute("href");
