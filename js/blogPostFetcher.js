b07e852e28229f907335045a019fff23d4f7fea9
Teddy <teddy.teh@ufinity.com>
2020-10-16 03:39:50 +0800

    Initial commit

---
+++ js/blogPostFetcher.js
@@ -0,0 +1,16 @@
+const RSS_URL = `https://blog.teddyteh.com/feed.xml`;
+
+fetch(RSS_URL)
+  .then(response => response.text())
+  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+  .then(data => {
+    const latestEntry = data.querySelectorAll("entry")[0];
+
+    if (latestEntry) {
+        const title = latestEntry.querySelector("title").innerHTML;
+        const link = latestEntry.querySelector("link").getAttribute("href");
+        
+        $("#latestBlogPost").text(title);
+        $("#latestBlogPost").attr("href", link);
+    }
+  });
